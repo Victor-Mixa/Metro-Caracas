1ade1fac208335be2f216bf677a3dc5447bda703
Victor <125752527+Victor-Mixa@users.noreply.github.com>
2023-02-24 16:38:31 -0400

    Metro_Caracas_R-1
    Correccion de texto y se agrega la seleccion de linea

--- Metro_Caracas_R-1.pas
+++ Metro_Caracas_R-1.pas
@@ -1,27 +1,27 @@
 program MetroC;
 uses crt;
 var
-Bol,A,B,C,D,E,F,G,H,I,J,nom,ape,Rep: string;
-CI, boletos,tipos,Num,Viajes: integer;
+Bol,nom,ape,rep: string;
+CI,A,B,C,D,E,F,G,H,I,J,tipos,Num,Viajes,line: integer;
 //INICIO DEL CODIGO//
 
 BEGIN
 ///asignacion de variables///
-A:='1= boleto simple amarillo, un viaje en metro';
-B:=('2= boleto integrado amarillo, un viaje en metro, un viaje en microbus');
-C:=('3= ida y vuelta, boleto amarillo, dos viajes en metro');
-D:=('4= ida y vuelta, boleto amarillo, 2 viajes en metro, 2 viajes en microbus');
-E:=('5= multiabono boleto naranja, 10 viajes en metro');
-F:=('6= MultiAbono integrado, boleto naranja, 10 viajes en metro, 10 viajes en microbus');
-G:=('7= Estudiantil simple, boleto nazul, 10 viajes en metro');
-H:=('8= Estudiantil integrado, boleto azul, 10 viajes en metro, 20 viajes en microbus');
-I:=('9= MetroTarjetas, boleto rojo, 20 viajes, 30 viajes, 40 viajes');
-J:=('10= MetroTarjeta integrada, boleto rojo, 20 viajes (metro y microbus), 30 viajes (metro y microbus, 40 viajes (metro y microbus9');
+A:=5;
+B:=10;
+C:=15;
+D:=20;
+E:=25;
+F:=30;
+G:=35;
+H:=40;
+I:=45;
+J:=50;
 
 		///comienzo del programa////
 writeln ('Bienvenido al Metro Caracas, desea comprar boletos?');
-readLn(Bol);
-if (Bol='si') then
+readLn(rep);
+if (rep='si') then
 	Begin
 		writeln ('porfavor digite su nombre');
 		readln (nom);
@@ -34,44 +34,86 @@ if (Bol='si') then
 ////Se enlista los tipos de boletos que hay/////
 writeln ('Elija el tipo de boleto que desea comprar');
 writeln();
-writeln(A);
-writeln(B);
-writeln(C);
-writeln(D);
-writeln(E);
-writeln(F);
-writeln(G);
-writeln(H);
-writeln(I);
-writeln(J);
+writeln ('1= boleto simple amarillo, un viaje en metro');
+writeln ('2= boleto integrado amarillo, un viaje en metro, un viaje en metrobus');
+writeln ('3= ida y vuelta, boleto amarillo, dos viajes en metro');
+writeln('4=ida y vuelta integrado, boleto amarillo, 2 viajes en metro, 2 viajes en metrobus');
+writeln('5=multiabono boleto naranja, 10 viajes en metro');
+writeln('6=MultiAbono integrado, boleto naranja, 10 viajes en metro, 10 viajes en metrobus');
+writeln('7=Estudiantil simple, boleto nazul, 10 viajes en metro');
+writeln ('8=Estudiantil integrado, boleto azul, 10 viajes en metro, 20 viajes en metrobus');
+writeln('9=MetroTarjetas, boleto rojo, 20 viajes, 30 viajes, 40 viajes');
+writeln('10=MetroTarjeta integrado, boleto rojo, 20 viajes (metro y metrobus), 30 viajes (metro y mitrobus), 40 viajes (metro y metrobus)');
 	Readln(tipos);
 clrscr;
 
-////se define los precios////
-writeln ('Usted a elegido el boleto: ',tipos);
-	case tipos of
-		1: writeln('El precio de este boleto es 5$');
-		2: writeln ('El precio de ste boleto es 10$');
-		3: writeln('El precio de este boleto es 15$');
-		4: writeln('El precio de este boleto es 20$');
-		5: writeln('El precio de este boleto es 25$');
-		6: writeln('El precio de este boleto es 30$');
-		7: writeln('El precio de este boleto es 35$');
-		8: writeln('El precio de este boleto es 40$');
-		9: writeln('El precio de este boleto es 45$');
-		10: writeln('El precio de este boleto es 50$');
+////se muestra la eleccion////
+case tipos of
+		1: writeln('Usted a elegido el boleto: simple');
+		2: writeln ('Usted a elegido el boleto: integrado');
+		3: writeln('Usted a elegido el boleto: ida y vuelva ');
+		4: writeln('Usted a elegido el boleto: ida y vuelta integrado');
+		5: writeln('Usted a elegido el boleto: multiabono');
+		6: writeln('Usted a elegido el boleto: multiabono integrado');
+		7: writeln('Usted a elegido el boleto: estudiantil simple');
+		8: writeln('Usted a elegido el boleto: estudiantil integrado');
+		9: writeln('Usted a elegido el boleto: metrotarjeta');
+		10: writeln('Usted a elegido el boleto: metrotarjeta integrado');
 		end;
+////se define los precios////
+	case tipos of
+		1: writeln('El precio de este boleto es ',A );
+		2: writeln ('El precio de ste boleto es ',B);
+		3: writeln('El precio de este boleto es ',C);
+		4: writeln('El precio de este boleto es ',D);
+		5: writeln('El precio de este boleto es ',E);
+		6: writeln('El precio de este boleto es ',F);
+		7: writeln('El precio de este boleto es ',G);
+		8: writeln('El precio de este boleto es ',H);
+		9: writeln('El precio de este boleto es ',I);
+		10: writeln('El precio de este boleto es ',J);
+		end;
+
 ///Muestra de los viajes del boleto////
 	case tipos of
-1: writeln ('usted tiene un viaje en metro');
-2: writeln('Usted tiene un viaje en metro y un viaje en microbus');
-3:writeln ('Usted tiene dos viajes en metro');
-4:writeln ('usted tiene 2 viajes en metro y 2 viajes en microbus');
-5:writeln ('usted tiene 10 viajes en metro');
-6:writeln ('usted tiene 10 viajes en metro y 10 viajes en microbus');
-7:writeln ('usted tiene 10 viajes en metro');
-8:writeln ('usted tiene 10 viajes en metro y 20 viajes en microbus');
+1: writeln ('Usted tiene un viaje en metro');
+2: writeln('Usted tiene un viaje en metro, un viaje en microbus');
+3:writeln ('Usted tiene 2 viajes en metro');
+4:writeln ('Usted tiene 2 viajes en metro, 2 viajes en microbus');
+5:writeln ('Usted tiene 10 viajes en metro');
+6:writeln ('Usted tiene 10 viajes en metro, 10 viajes en microbus');
+7:writeln ('Usted tiene 10 viajes en metro');
+8:writeln ('Usted tiene 10 viajes en metro, 20 viajes en microbus');
 	end;
+		
+/////Se pregunta la cantidad de boletos////
+WriteLn('Digite la cantidad de boletos a comprar');
+readln(Num);
+
+		if tipos=2 then
+		begin
+		writeln('En que transporte realizara el viaje, en metro o metrobus?');
+		readln(bol);
+		end;
+		
+		if tipos=4 then 
+		begin
+		writeln('En que transporte realizara el viaje, en metro o metrobus?');
+		readln(bol);
+		end;
+		
+		if tipos=6 then
+		begin
+		writeln('En que transporte realizara el viaje, en metro o metrobus?');
+		readln(bol);
+		end;
+		
+		if tipos=8 then
+		begin
+		writeln('En que transporte realizara el viaje, en metro o metrobus?');
+		readln(bol);
+		end;
+		
 		if tipos=(9) then
 			begin
 			WriteLn('cuantos viajes desea?');
@@ -90,10 +132,12 @@ writeln ('Usted a elegido el boleto: ',tipos);
 			WriteLn('1: 20 viajes, 2: 30 viajes, 3:40 viajes');
 			readLn(viajes);
 				case viajes of
-					1: writeln('usted tiene 20 viajes en metro y microbus');
-					2: writeln('usted tiene 30 viajes en metro y microbus');
-					3: writeln('usted tiene 40 viajes en metro y microbus')
-				end;
+					1: writeln('usted tiene 20 viajes en metro');
+					2: writeln('usted tiene 30 viajes en metro');
+					3: writeln('usted tiene 40 viajes en metro')
+			end;
+		writeln('En que transporte realizara el viaje, en metro o metrobus?');
+		readln(bol);
 		end;
 		
 ///confirmacion para continuar///
@@ -101,25 +145,40 @@ writeln ('Estas seguro de continuar?');
 	readln(Rep);
 	if (Rep='si') then 
 		Begin
-			WriteLn('Digite la cantidad de boletos a comprar');
-			readln(Num)
+
 		end;
 		
 	///se le muestra al usuario lo pedido///
 	case tipos of
-		1: writeln('El precio de este boleto es ',5*Num,'$');
-		2: writeln ('El precio de ste boleto es ',10*Num,'$');
-		3: writeln('El precio de este boleto es ',15*Num,'$');
-		4: writeln('El precio de este boleto es ',20*Num,'$');
-		5: writeln('El precio de este boleto es ',25*Num,'$');
-		6: writeln('El precio de este boleto es ',30*Num,'$');
-		7: writeln('El precio de este boleto es ',35*Num,'$');
-		8: writeln('El precio de este boleto es ',5*Num,'$');
-		9: writeln('El precio de este boleto es ',45*Num,'$');
-		10: writeln('El precio de este boleto es ',50*Num,'$');
+		1: writeln('El precio de este boleto es ',A*Num,'$');
+		2: writeln ('El precio de ste boleto es ',B*Num,'$');
+		3: writeln('El precio de este boleto es ',C*Num,'$');
+		4: writeln('El precio de este boleto es ',D*Num,'$');
+		5: writeln('El precio de este boleto es ',E*Num,'$');
+		6: writeln('El precio de este boleto es ',F*Num,'$');
+		7: writeln('El precio de este boleto es ',G*Num,'$');
+		8: writeln('El precio de este boleto es ',H*Num,'$');
+		9: writeln('El precio de este boleto es ',I*Num,'$');
+		10: writeln('El precio de este boleto es ',J*Num,'$');
 	end;
 
-end
+end;
+
+WriteLn('Desde la linea 1 hatas la linea 8.En que linea desea viajar?');
+Writeln('Digite un numero del 1 al 8');
+Readln(Line);
+	case Line of
+		1:WriteLn('Propatria,Perez Bonalde, Plaza Sucre, Gato Negro, Agua Salud, Caño Amarillo,   Capitolio, La Hoyada, Parque Carabobo, Bellas Artes, Colegio de ing., Plaza     Venezuela, Sabana Grande, Chacaito, Chacao, Altamira, Miranda, Los Dos Caminos, Los Cortijos, La California, Petare, Palo Verde');
+		2:WriteLn('El silencio, Capuchinos, Maternidad, Artiguas, La Paz, La yaguara, Carapita,    Antimano, Mamera, Caricuao, Zoologico, Ruiz Pineda/Las juntas');
+		3:WriteLn('Plaza Venezuela, Ciudad Universitaria, Los símbolos, La bandera, El Valle, Los jardines, Coche ,Mercado, La Rinconada');
+		4:WriteLn('Zona rental, Parque Central, Nuevo Circo, Teatro, Capuchinos, Maternidad, Artigas, La Paz, La Yaragua, Carapita, Antimano, Mamera, Ruiz Pineda/Las Adjuntas');
+		5:WriteLn('Bello Monte, Las Mercedes, Parque Simón Bolívar, Bello Campo, Hugo Chávez, Monte Cristo, Boleita, El Marquéz, Warairarepano');
+		6:WriteLn('Zoológico,La Riconada');
+		7:WriteLn('Las flores, Panteón, Socorro, La Hoyada, El Cristo, Roca torpeya, Presidente Medina, INCES, Roosevelt, La Bandera, Los Ilustres');
+		8:WriteLn('Petare 2, 19 de abril, 5 de julio, 24 de julio, Warairarepano');
+	else 
+		writeln('esa no es una linea valida');
+		end;
 
 ///En caso de que el usuario NO quiera comprar el boleto///
 Else
