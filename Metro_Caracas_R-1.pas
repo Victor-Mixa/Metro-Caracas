e7906ac9d26097376a02d8ed909cc48b9c48272b
Victor <125752527+Victor-Mixa@users.noreply.github.com>
2023-02-24 20:43:36 -0400

    Metro_Caracas_r-1
    Actualización de variables y mejora de opciones ya establecidas

--- Metro_Caracas_R-1.pas
+++ Metro_Caracas_R-1.pas
@@ -2,11 +2,11 @@ program MetroC;
 uses crt;
 var
 Bol,nom,ape,rep: string;
-CI,A,B,C,D,E,F,G,H,I,J,tipos,Num,Viajes,line: integer;
+CI,A,B,C,D,E,F,G,H,I,J,tipos,Num,Viajes,line,buc,totalv,VA,VC,VE,P,M,Gr: integer;
 //INICIO DEL CODIGO//
 
 BEGIN
-///asignacion de variables///
+///asignacion de precios///
 A:=5;
 B:=10;
 C:=15;
@@ -17,6 +17,15 @@ G:=35;
 H:=40;
 I:=45;
 J:=50;
+///asignacion de viajes////
+VA:=1;
+VC:=2;
+VE:=10;
+P:=20;
+M:=30;
+Gr:=40;
+totalv:=0;
+
 
 		///comienzo del programa////
 writeln ('Bienvenido al Metro Caracas, desea comprar boletos?');
@@ -74,36 +83,36 @@ case tipos of
 		10: writeln('El precio de este boleto es ',J);
 		end;
 
-///Muestra de los viajes del boleto////
-	case tipos of
-1: writeln ('Usted tiene un viaje en metro');
-2: writeln('Usted tiene un viaje en metro, un viaje en microbus');
-3:writeln ('Usted tiene 2 viajes en metro');
-4:writeln ('Usted tiene 2 viajes en metro, 2 viajes en microbus');
-5:writeln ('Usted tiene 10 viajes en metro');
-6:writeln ('Usted tiene 10 viajes en metro, 10 viajes en microbus');
-7:writeln ('Usted tiene 10 viajes en metro');
-8:writeln ('Usted tiene 10 viajes en metro, 20 viajes en microbus');
-	end;
-		
 /////Se pregunta la cantidad de boletos////
 WriteLn('Digite la cantidad de boletos a comprar');
 readln(Num);
 
+///Muestra de los viajes del boleto////
+	case tipos of
+1:writeln ('Usted tiene ',VA*num,' viajes en metro');
+3:writeln ('Usted tiene ',VC*num,' viajes en metro');
+5:writeln ('Usted tiene ',VE*num,' viajes en metro');
+7:writeln ('Usted tiene ',VE*num,' viajes en metro');
+8:writeln ('Usted tiene 10 viajes en metro, 20 viajes en microbus');
+	end;
+
 		if tipos=2 then
 		begin
+		Writeln('Usted tiene ',VA*Num,' viajes en metro');
 		writeln('En que transporte realizara el viaje, en metro o metrobus?');
 		readln(bol);
 		end;
 		
 		if tipos=4 then 
 		begin
+		Writeln('Usted tiene ',VC*Num,' viajes en metro');
 		writeln('En que transporte realizara el viaje, en metro o metrobus?');
 		readln(bol);
 		end;
 		
 		if tipos=6 then
 		begin
+		writeln ('Usted tiene ',VE*num,' viajes en metro');
 		writeln('En que transporte realizara el viaje, en metro o metrobus?');
 		readln(bol);
 		end;
@@ -112,41 +121,84 @@ readln(Num);
 		begin
 		writeln('En que transporte realizara el viaje, en metro o metrobus?');
 		readln(bol);
+			if bol='metro' then
+			begin
+			writeln ('Usted tiene ',VE*num,' viajes en metro');
+			end
+			else writeln ('Usted tiene ',P*num,' viajes en metrobus');
 		end;
 		
 		if tipos=(9) then
 			begin
-			WriteLn('cuantos viajes desea?');
-			WriteLn('1: 20 viajes, 2: 30 viajes, 3:40 viajes');
-			readLn(viajes);
-				case viajes of
-					1: writeln('usted tiene 20 viajes en metro');
-					2: writeln('usted tiene 30 viajes en metro');
-					3: writeln('usted tiene 40 viajes en metro')
-			end;
-		end;
+				for buc:=1 to num do
+					begin
+					WriteLn('Para su boleto ',buc,' cuantos viajes desea?');
+					WriteLn('1: 20 viajes, 2: 30 viajes, 3: 40 viajes');
+					readLn(viajes);
+						case viajes of
+							1: Totalv:=Totalv+P;
+							2: Totalv:=Totalv+M;
+							3: Totalv:=Totalv+Gr;
+						end;
+					while viajes>3 do
+						begin
+						writeln('Numero de viajes invalido');
+						WriteLn('Para su boleto ',buc,' cuantos viajes desea?');
+						WriteLn('1: 20 viajes, 2: 30 viajes, 3: 40 viajes');
+						readLn(viajes);
+							case viajes of
+								1: Totalv:=Totalv+P;
+								2: Totalv:=Totalv+M;
+								3: Totalv:=Totalv+Gr;
+							end;
+						clrscr;
+						end;
+
+					WriteLn('Tiene ',totalv,' viajes en total');
+					readln();
+					clrscr;
+					end;
+				end;
 		
 		if tipos=(10) then
 			begin
-			WriteLn('cuantos viajes desea?');
-			WriteLn('1: 20 viajes, 2: 30 viajes, 3:40 viajes');
-			readLn(viajes);
-				case viajes of
-					1: writeln('usted tiene 20 viajes en metro');
-					2: writeln('usted tiene 30 viajes en metro');
-					3: writeln('usted tiene 40 viajes en metro')
-			end;
+				for buc:=1 to num do
+					begin
+					WriteLn('Para su boleto ',buc,' cuantos viajes desea?');
+					WriteLn('1: 20 viajes, 2: 30 viajes, 3: 40 viajes');
+					readLn(viajes);
+						case viajes of
+							1: Totalv:=Totalv+P;
+							2: Totalv:=Totalv+M;
+							3: Totalv:=Totalv+Gr;
+						end;
+					while viajes>3 do
+						begin
+						writeln('Numero de viajes invalido');
+						WriteLn('Para su boleto ',buc,' cuantos viajes desea?');
+						WriteLn('1: 20 viajes, 2: 30 viajes, 3: 40 viajes');
+						readLn(viajes);
+							case viajes of
+								1: Totalv:=Totalv+P;
+								2: Totalv:=Totalv+M;
+								3: Totalv:=Totalv+Gr;
+							end;
+						clrscr;
+						end;
+
+					WriteLn('Tiene ',totalv,' viajes en total');
+					readln();
+					clrscr;
+					end;
 		writeln('En que transporte realizara el viaje, en metro o metrobus?');
 		readln(bol);
 		end;
-		
 ///confirmacion para continuar///
 writeln ('Estas seguro de continuar?');
 	readln(Rep);
+	clrscr;
 	if (Rep='si') then 
 		Begin
-
-		end;
 		
 	///se le muestra al usuario lo pedido///
 	case tipos of
@@ -162,8 +214,9 @@ writeln ('Estas seguro de continuar?');
 		10: writeln('El precio de este boleto es ',J*Num,'$');
 	end;
 
-end;
 
+/////Seleccion de la linea/////
+writeln();
 WriteLn('Desde la linea 1 hatas la linea 8.En que linea desea viajar?');
 Writeln('Digite un numero del 1 al 8');
 Readln(Line);
@@ -172,18 +225,23 @@ Readln(Line);
 		2:WriteLn('El silencio, Capuchinos, Maternidad, Artiguas, La Paz, La yaguara, Carapita,    Antimano, Mamera, Caricuao, Zoologico, Ruiz Pineda/Las juntas');
 		3:WriteLn('Plaza Venezuela, Ciudad Universitaria, Los símbolos, La bandera, El Valle, Los jardines, Coche ,Mercado, La Rinconada');
 		4:WriteLn('Zona rental, Parque Central, Nuevo Circo, Teatro, Capuchinos, Maternidad, Artigas, La Paz, La Yaragua, Carapita, Antimano, Mamera, Ruiz Pineda/Las Adjuntas');
-		5:WriteLn('Bello Monte, Las Mercedes, Parque Simón Bolívar, Bello Campo, Hugo Chávez, Monte Cristo, Boleita, El Marquéz, Warairarepano');
-		6:WriteLn('Zoológico,La Riconada');
+		5:WriteLn('Bello Monte, Las Mercedes, Parque Simón Bolívar, Bello Campo, Hugo Chávez, Monte Cristo, Boleita, El Marquez, Warairarepano');
+		6:WriteLn('Zoologico,La Riconada');
 		7:WriteLn('Las flores, Panteón, Socorro, La Hoyada, El Cristo, Roca torpeya, Presidente Medina, INCES, Roosevelt, La Bandera, Los Ilustres');
-		8:WriteLn('Petare 2, 19 de abril, 5 de julio, 24 de julio, Warairarepano');
-	else 
-		writeln('esa no es una linea valida');
-		end;
+		8:WriteLn('Petare 2, 19 de abril, 5 de julio, 24 de julio, Warairarepano')
+		else 
+			writeln('esa no es una linea valida');
+		end
+	end;
 
-///En caso de que el usuario NO quiera comprar el boleto///
-Else
-Begin
-WriteLn('que tenga buen dia');
+
+Writeln('ingrese el nombre de la estacion de salida');
+readln()
+
 End
+
+//////En caso de que la persona NO quiera comprar un boleto///////
+Else
+WriteLn('que tenga buen dia');
 
 END.
